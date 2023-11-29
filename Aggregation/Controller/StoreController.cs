99ecec3e9c28248259a21bac7bfd30cb65ffa582
Vania98 <125358275+Vania98@users.noreply.github.com>
2023-11-29 21:20:05 +0200

    Add files via upload

---
+++ Aggregation/Controller/StoreController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Aggregation.Model;
+using Aggregation.Services;
+
+namespace Aggregation.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private readonly IStore_1Service _store_1Service;
+        private readonly IStoreProgram_Service _storeProgram_Service;
+
+        public StoreController(IStore_1Service store_1Service, IStoreProgram_Service storeProgram_Service)
+        {
+            _store_1Service = store_1Service;
+            _storeProgram_Service = storeProgram_Service;
+        }
+        [HttpGet("GetAll")]
+        public async Task<GetAllModel> GetAll()
+        {
+            var products = await _store_1Service.GetProducts();
+            var sellers = await _store_1Service.GetSeller();
+            var baskets = await _storeProgram_Service.GetBaskets();
+            var clients = await _storeProgram_Service.GetClients();
+
+            var result = new GetAllModel
+            {
+                Product = products,
+                Seller = sellers,
+                Basket = baskets,
+                Client = clients
+            };
+
+            return result;
+        }
+
+    }
+}
---
+++ Aggregation/Program.cs
@@ -0,0 +1,34 @@
+using Aggregation.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+
+builder.Services.AddHttpClient<IStore_1Service, Store_1Service>
+    (c => c.BaseAddress = new Uri("http://localhost:5263"));
+builder.Services.AddHttpClient<IStoreProgram_Service, StoreProgram_Service>
+    (c => c.BaseAddress = new Uri("http://localhost:5050"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
